a266ea96635f0e074f076ca3ed266728779b6b34
JuanitaAK <afanadorjuanita@gmail.com>
2023-01-05 14:44:03 +0100

    changes main page background and image

--- SurfSpot/SurfSpot/ContentView.swift
+++ SurfSpot/SurfSpot/ContentView.swift
@@ -9,15 +9,31 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "figure.surfing")
-                .imageScale(.large)
-                .foregroundColor(.red)
-            Text("Surf Spot!")
-            .bold()
-            .italic()
+        
+        ZStack {
+            Color(.black)
+                .ignoresSafeArea()
+            VStack {
+                
+                Image(systemName: "figure.surfing")
+                    .imageScale(.large)
+                    .foregroundColor(Color.white)
+                Image("SurfBoard")
+                    .resizable()
+                    .cornerRadius(40)
+                    .aspectRatio(contentMode: .fit)
+                    .padding()
+                Text("Surf Spot!")
+                    .font(/*@START_MENU_TOKEN@*/.largeTitle/*@END_MENU_TOKEN@*/)
+                    .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                    .foregroundColor(Color.white)
+                    .bold()
+                    .italic()
+            
+            }
+            
         }
-        .padding()
+
     }
 }
 
