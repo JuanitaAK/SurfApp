08d97db80f4d1bc5ff9b3958e61fc513a15b6bdc
JuanitaAK <afanadorjuanita@gmail.com>
2023-01-20 21:30:11 +0100

    Images and texte aligned in the list

--- SurfSpot/SurfSpot/Views/SpotListView.swift
+++ SurfSpot/SurfSpot/Views/SpotListView.swift
@@ -14,53 +14,68 @@ struct SpotListView: View {
     var body: some View {
         
         List(records, id:  \.id){ item in
-           // VStack(alignment: .leading){
-                //NavigationLink(destination: SpotListView(), label: {
+            // VStack(alignment: .leading){
+            //NavigationLink(destination: SpotListView(), label: {
+            
+            VStack(alignment: .center, spacing: -12.0){
                 
-            VStack(alignment: .center){
-                 Spacer()
-                    HStack{
-                        //"https://dl.airtable.com/ZuXJZ2NnTF40kCdBfTld_thomas-ashlock-64485-unsplash.jpg"
-                        AsyncImage(
-                            url:URL(string:item.fields.photos[0].url)){ phase in
-                                if let image = phase.image {
-                                    image
-                                        .resizable()
-                                        .scaledToFit()
-                                } else if phase.error != nil {
-                                    Text("There was an error loading the image.")
-                                } else {
-                                    ProgressView()
-                                }
-                            }
-                            .frame(width: 100, height: 100)
-                            
-                        
-                        VStack(alignment: .leading){
-                            //Title of the spot
-                            Text(item.fields.surfBreak[0])
-                                .font(.title2)
-                                .foregroundColor(Color.black)
-                                .padding([.top, .leading, .bottom], 3.0)
-                                .bold()
-                            //Adress of the spot
-                            Text(item.fields.address )
-                                .scaledToFit()
-                        }
-                        .scaledToFit()
-                    
-                    }
                 Spacer()
-                    .padding(.leading, 16.0)
+              
+                HStack(spacing: 19.0){
+                    
+                    
+                    AsyncImage(
+                        url:URL(string:item.fields.photos[0].url)){ phase in
+                            if let image = phase.image {
+                                image
+                                    .resizable()
+                                    .scaledToFit()
+                            } else if phase.error != nil {
+                                Text("There was an error loading the image.")
+                            } else {
+                                ProgressView()
+                            }
+                        }
+                        
+                        .frame(width: 100, height: 100)
+                        .padding([.top, .leading, .bottom], -2.0)
+                    
+                    
+                    
+                    VStack(alignment: .leading){
+                        //Title of the spot
+                        Text(item.fields.surfBreak[0])
+                            .font(.title2)
+                            .foregroundColor(Color.black)
+                            .padding([.top, .leading, .bottom], 1.0)
+                            .bold()
+                        
+                        //Adress of the spot
+                        Text(item.fields.address )
+                            
+                    }
+                    .padding(.leading, 6.0)
+                    //.scaledToFit()
+                    
+                Spacer()
+                    
+                }
+                .scaledToFill()
+                .padding([.top, .leading, .bottom], 2.0)
+               
+                Spacer()
+                 
+                //padding(.leading, 1.0)
             }
+            .padding(.leading)
+            .scaledToFit()
+            .frame(width: 350.0, height:100)
+            .background(Rectangle()
+                .foregroundColor(Color.white))
+            .cornerRadius(10)
+            .shadow(radius: 4)
             //.padding(.leading, 1.0)
-                .scaledToFit()
-                .frame(width: 350.0, height:100)
-                .background(Rectangle()
-                    .foregroundColor(Color.white))
-                .cornerRadius(10)
-                .shadow(radius: 4)
-          
+           
         }
         .task {
             await decodeAPI()
